bc0a9e4fd7e2c7afa0704b9cd58918f9dfdad2f5
Yordan Valkov <yordan.valkov@gmail.com>
2024-06-25 10:41:30 +0300

    Edit production link UI test

--- tests/ui/shoppingList.test.js
+++ tests/ui/shoppingList.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check shopping list page', async ({ page }) => {
-    await page.goto('http://localhost:8080/shopping-list');
+    await page.goto('https://software-engineering-devops-examprep-q73e.onrender.com/shopping-list');
     const list = await page.$('li');
     expect(list).toBeTruthy();
   });
